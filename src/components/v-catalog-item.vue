1e7cb96d91394b7781aed223d99477f31d9e34a8
TatyanaKonina <tdkonina@edu.hse.ru>
2020-11-03 20:02:39 +0300

    start work

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class = "v-catalog-item">
+    <p>item 1 </p>
+
+    
+    
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:"v-catalog-item",
+    props:{},
+    data(){
+        return{}
+    },
+    computed:{}
+}
+</script>
+
+<style lang="scss">
+.v-catalog-item {
+    flex-basis: 25%;
+    box-shadow: 0 0 8px 0 grey;
+    padding: 16px;
+    margin-bottom: 16px;
+}
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,57 @@
+<template>
+<div class ="v-main-wrapper">
+    
+    <p>{{title}} </p>
+    <v-catalog />
+    <v-cart />
+
+</div>
+
+</template>
+
+<script>
+
+import vCatalog from './v-catalog.vue'
+
+import vCart from './v-cart'
+
+export default {
+    name: "v-main-wrapper",
+    components:{
+        vCatalog,vCart
+    },
+    props :{
+
+    },
+    data(){
+        return {
+            title : "Main Wrapper"
+        }
+    },
+    coputed:{
+
+    },
+    methods:{
+
+    },
+    watch:{
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+
+<style>
+.v-main-wrapper{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 900px;
+    margin:0 auto;
+
+}
+
+
+</style>
