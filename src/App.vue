a27f34a64d3d93c718b9462a6419b2060bd32e1e
TatyanaKonina <tdkonina@edu.hse.ru>
2020-11-12 10:51:12 +0300

    try  select list

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-<div id ="app">
 
-  <v_main_wrapper/>
+<div id ="app">
+  <v-select
+       :options="options"
+
+
+
+  />
+
+
+  <!-- <v_main_wrapper/> -->
 
 </div>
 
 </template>
 
 <script>
+import vSelect from './components/v-select'
 import v_main_wrapper from "./components/v-main-wrapper"
 export default {
   name: 'App',
   components: {
-    v_main_wrapper
+   // v_main_wrapper,
+   vSelect
+  },
+  data(){
+    return {
+        options:[
+          {name: 'Option 1',value: 1},
+          {name: 'Option 2',value: 2},
+          {name: 'Option 3',value: 3},
+          {name: 'Option 4',value: 4}
+        ]
+    }
   }
 }
 </script>
---
+++ src/components/v-select.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="v-select">
+        <p class="title"
+             @click="areOptionsVisible= !areOptionsVisible" 
+        
+        
+        
+        >Select</p>
+        <div 
+            class="options"
+            v-if="areOptionsVisible"
+
+        >
+
+            <p
+                v-for='option in options'
+                :key="option.value"
+
+            >
+                {{option.name}}
+
+            </p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "v-select",
+    props:{
+        options:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+    },
+    data(){
+        return {
+            areOptionsVisible : false
+        }
+    },
+    computed:{}
+}
+</script>
+
+<style scoped>
+.v-select{
+    position:relative;
+    width:200px;
+    cursor: pointer;
+}
+.title {
+    border:solid 1px;
+}
+.v-select p {
+    margin: 0
+}
+.options {
+    border:solid 1px grey;
+    position:absolute;
+    top : 30px;
+    right:0;
+    width:100%;
+
+}
+
+</style>
