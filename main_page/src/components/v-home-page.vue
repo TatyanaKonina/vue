fffccfe2f32c793881e445e1e7db95d1bf78cf08
TatyanaKonina <tdkonina@edu.hse.ru>
2020-11-14 12:55:54 +0300

    finish main_page

--- main_page/src/components/v-home-page.vue
+++ main_page/src/components/v-home-page.vue
@@ -7,12 +7,40 @@
           <li class="link">About us</li>
           <li class="link">Our servises</li>
           <li class="link">Contacts</li>
-          <li class="link">потом придумаю</li>
+          <li class="link">Want to join us?</li>
         </ul>
         <ul class="nav icon">
           <li class="link"><font-awesome-icon :icon="['fa','search']"/></li>
-          <li class="link"><font-awesome-icon :icon="['fa','shopping-cart']"/> </li>
+          <li class="link"><font-awesome-icon :icon="['fa','shopping-cart']"/></li>
           <li class="link"><font-awesome-icon :icon="['fa','user']"/></li>
+        </ul>
+        </div>
+      </div>
+      <div class="description">
+        <h1>Our Company</h1>
+        <p>The outsourcing company provides personnel outsourcing services to organizations,
+        those who need temporary employees of a certain profession and qualification.
+        At the request of the employer, the staff is selected, trained, certified and staffed
+        performers (a group of 15-20 people), and then transferred to the order of the customer for rent
+        agreement.  
+        </p>
+        <br>
+        <div class="botton">
+          <h2 class="link">Find Team</h2>
+        </div>
+    </div>
+    <div class="end-head">
+      <div class="post-navigation">
+        <ul class="social">
+          <li class="link"><font-awesome-icon :icon="['fab','facebook-square']"/></li>
+          <li class="link"><font-awesome-icon :icon="['fab','twitter']"/></li>
+          <li class="link"><font-awesome-icon :icon="['fab','vk']"/></li>
+          <li class="link"><font-awesome-icon :icon="['fab','instagram']"/></li>
+        </ul>
+        <ul class="post-nav">
+          <li class="link">Garanties</li>
+          <li class="link">Support Center</li>
+          <li class="link">Privacy</li>
         </ul>
       </div>
     </div>
@@ -38,11 +66,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Modak&family=Roboto&family=Tajawal:wght@900&display=swap');
+//@import url('https://fonts.googleapis.com/css2?family=Modak&family=Roboto&family=Tajawal:wght@900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Fredoka+One&display=swap');
 .v-home-page{
   margin:0;
   padding: 0;
-  font-family: 'Roboto', sans-serif;
+  font-family: 'Fredoka One', cursive;
   height: 100vh;
   background: linear-gradient(rgba(1,3,56,.8),rgba(1,3,56,.4)),url('../assets/images/background.jpg');
   -webkit-background-size: cover;
@@ -73,7 +102,7 @@ export default {
   color:#fff;
   text-decoration: none;
   min-height: 0 30px 0 0;
-  font-size: 20px;
+  font-size: 30px;
   margin: 0 30px 0 0;
 }
 .icon li {
@@ -82,15 +111,83 @@ export default {
 }
 .logo{
   font-size: 50px;
-  font-family: 'Modak', cursive;
+  font-family: 'Fredoka One', cursive;
   color:rgb(212, 0, 159);
 
 }
 .link{
   transition: .3s;
+  cursor: pointer;
 }
 .link:hover{
   color: rgb(212, 0, 159);
 }
+.description{
+  top:35%;
+  left: 5%;
+  width:40%;
+  position:absolute;
+  text-align: left;
+
+}
+.description h1{
+  margin: 0;
+  font-size: 40px;
+  color:#fff;
+  font-weight:900;
+  font-family: 'Fredoka One', cursive;
+}
+.description p{
+  color:rgb(177, 94, 245);
+  font-size: 25px;
+  font-family: 'Abril Fatface', cursive;
+}
+.botton{
+  background:none;
+  border:3px solid #fff;
+  text-decoration: none;
+  color: #fff;
+  font-size: 16px;
+  text-transform:upper-case;
+  padding: 10 px 25 px;
+  border-radius: 7px;
+  font-weight: bold;
+  transition: .4s;
+  text-align: center;
+}
+.post-navigation{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  position: absolute;
+  top:98%;
+  left:50%;
+  transform: translate(-50%, -50%);
+}
+.social{
+  display: flex;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+.social li{
+  padding: 0 35px 0 0;
+  font-size: 40px;
+  text-decoration: none;
+  color: #c7c7c7
+}
+.post-nav{
+  display: flex;
+  list-style:none
+}
+.post-nav li{
+  padding: 0 35px 0 0;
+  font-size: 30px;
+  text-decoration: none;
+  color: #c7c7c7
+}
+
+
 }
 </style>
--- main_page/src/main.js
+++ main_page/src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fas} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-
+import { fab } from '@fortawesome/free-brands-svg-icons'
 Vue.config.productionTip = false
-library.add(fas);
+library.add(fas,fab);
+
 Vue.component('font-awesome-icon',FontAwesomeIcon)
 
 new Vue({
